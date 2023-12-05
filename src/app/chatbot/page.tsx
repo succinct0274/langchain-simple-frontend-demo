ed1a08d76f184bb586fba1b39170e517646b4303
Kelvin Kwong <kelvin.kwong@altech.hk>
2023-12-05 23:16:38 +0800

    non uuid grud

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -1,13 +1,22 @@
 import { fetchConversationHistory } from "@/lib/fetchConversationHistory";
+import { z } from "zod";
 import DescriptionWithFloatingButton from "./description-with-floating-button";
-
+import { redirect } from "next/navigation";
+const uuidS = z.string().uuid();
 type Props = {
   searchParams: { uuid: string };
 };
 
 export default async function Home({ searchParams }: Props) {
   const { uuid } = searchParams;
+  if (!uuidS.safeParse(uuid).success) {
+    //redirect to chat page
+    redirect("/chat");
+  }
   const { messages, conversationId } = await fetchConversationHistory(uuid);
+
+  //chat params is uuid or not
+
   return (
     <DescriptionWithFloatingButton
       messages={messages}
