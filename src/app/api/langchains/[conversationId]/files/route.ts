dc94ab6a72e61b9377857a38fa7eba88376faae7
Kelvin Kwong <kelvin.kwong@altech.hk>
2023-12-06 17:52:52 +0800

    no cache

--- src/app/api/langchains/[conversationId]/files/route.ts
+++ src/app/api/langchains/[conversationId]/files/route.ts
@@ -4,10 +4,15 @@ const LANGCHAIN_SERVER_URL = process.env.LANGCHAIN_SERVER_URL;
 
 export const revalidate = 0;
 
-export async function GET(req: NextRequest, { params }: { params: { conversationId: string }}) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { conversationId: string } }
+) {
   const { conversationId } = params;
 
   return fetch(`${LANGCHAIN_SERVER_URL}/langchains/${conversationId}/files`, {
-    method: 'GET',
+    method: "GET",
+    next: { revalidate: 0 },
+    cache: "no-cache",
   });
 }
